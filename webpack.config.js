7a0432491d884d04be0edba03ecd020ca8129554
Jaroslav Rusanov <sco.nuff@gmail.com>
2024-05-14 12:51:15 +0300

    fix Tutors code review 02 (fix webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,16 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
